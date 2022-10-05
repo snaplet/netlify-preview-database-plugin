3162d7d62b196c22b044a77c4a65271f33ee4e5b
jgoux <hi@jgoux.dev>
2022-10-05 20:06:55 +0200

    don't fail the process for production environment

--- src/index.js
+++ src/index.js
@@ -13,7 +13,13 @@ export async function onPreBuild(config) {
       await handleDeployPreview(config);
       break;
     case "production":
-      await handleDeployProduction(config);
+      try {
+        await handleDeployProduction(config);
+      } catch (e) {
+        // We don't want the production build to fail because of resources clean up
+        console.error(`Failed to clean up resources for this commit`);
+        console.error(e);
+      }
       break;
     default:
   }
