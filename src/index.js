7996a5b16b6c9681af8c16c7adb9f2ea778ff86f
jgoux <hi@jgoux.dev>
2022-10-05 14:05:56 +0200

    add default values into the manifest

--- src/index.js
+++ src/index.js
@@ -2,26 +2,17 @@ import { createPreviewDatabase } from "./createPreviewDatabase.js"
 import { installSnapletCLI } from "./installSnapletCLI.js";
 import { setEnvironmentVariable } from "./setEnvironmentVariable.js";
 
-const DEFAULT_INPUTS = {
-  databaseEnvVar: "DATABASE_URL",
-  databaseCreateCommand: "snaplet db create --git --latest",
-  databaseUrlCommand: "snaplet db url --git",
-  reset: false,
-};
-
 export async function onPreBuild({
   utils: { run },
   constants,
   netlifyConfig,
-  inputs,
-}) {
-  const {
+  inputs: {
     databaseEnvVar,
     databaseCreateCommand,
     databaseUrlCommand,
     reset,
-  } = { ...DEFAULT_INPUTS, ...inputs };
-
+  },
+}) {
   if (process.env.CONTEXT === "deploy-preview") {
     await installSnapletCLI({ run });
 
